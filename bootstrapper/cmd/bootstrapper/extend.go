42013647a12d306098dfb691a883aad39b14442b
Houadef Salih <salih.houadef@gmail.com>
2024-03-09 20:41:12 +0100

    fix: Add timeout and error handling for kubectl apply with logging for troubleshooting

--- bootstrapper/cmd/bootstrapper/extend.go
+++ bootstrapper/cmd/bootstrapper/extend.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -13,6 +16,7 @@ const (
 	kubeConfigEnv             = "KUBECONFIG"
 	nginxTimeoutArg           = "--timeout=800s"
 	bootstrapperSleepInterval = 100 * time.Second
+	commandTimeout            = 2 * time.Minute
 )
 
 func applyCustomManifest() {
@@ -44,18 +48,49 @@ func isClusterReady() bool {
 }
 
 func applyManifest() {
-	// Check if the output contains the desired string
-	log.Println("K8s cluster initliazed, applying chart...")
-	cmd := exec.Command("kubectl", "apply", "--server-side", "-f", manifest)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Printf("Failed to apply Template using kubectl: %v\nOutput: %s", err, string(out))
-		cmd := exec.Command("kubectl", "wait", "--namespace", "keda", "--for=condition=available", "deployment/ingress-nginx-controller", nginxTimeoutArg)
-		err := cmd.Run()
-		if err != nil {
-			log.Printf("Failed waiting for nginx controller to start")
+	ctx, cancel := context.WithTimeout(context.Background(), commandTimeout)
+	defer cancel()
+	cmd := exec.CommandContext(ctx, "kubectl", "apply", "--server-side", "-f", manifest)
+	var outBuf, errBuf bytes.Buffer
+	cmd.Stdout = &outBuf
+	cmd.Stderr = &errBuf
+
+	if err := startTimedProcess(cmd, commandTimeout); err != nil {
+		log.Printf("kubectl apply output: %s\n", outBuf.String())
+		log.Printf("kubectl apply error: %s\n", errBuf.String())
+
+		log.Printf("Failed to apply Template using kubectl: %v\n", err)
+
+		waitForNginxController()
+	}
+}
+
+func startTimedProcess(cmd *exec.Cmd, timeout time.Duration) error {
+	go func() {
+		<-time.After(timeout)
+		if err := cmd.Process.Signal(os.Interrupt); err != nil {
+			fmt.Println("Failed to send interrupt signal:", err)
 		}
+	}()
+	err := cmd.Start()
+	if err != nil {
+		fmt.Printf("Failed starting command %v", cmd.Path)
+		return err
+	}
+	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func waitForNginxController() {
+	cmd := exec.Command("kubectl", "wait", "--namespace", "keda", "--for=condition=available", "deployment/ingress-nginx-controller", nginxTimeoutArg)
+	// Execute the command
+	log.Println("Waiting for nginx controller...")
+	if err := cmd.Run(); err != nil {
+		log.Printf("Failed waiting for nginx controller to start: %v\n", err)
+		// Retry applying the manifest
 		applyManifest()
 	}
-	return
 }
